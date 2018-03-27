c78cd8210f999b15f82abdaf1b9708618a3891c2
Kristin Henno <kristinhenno@gmail.com>
2018-03-27 11:29:58 -0700

    first commit

---
+++ js/portfolio.js
@@ -0,0 +1,20 @@
+  jQuery(document).ready(function($) {
+ 
+        $('#myCarousel').carousel({
+          
+        });
+ 
+        //Handles the carousel thumbnails
+        $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+        // When the carousel slides, auto update the text
+  
+});
